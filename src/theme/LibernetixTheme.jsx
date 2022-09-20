54bb73124bd8224a4a3193636e02e2dfd70cf234
Vsevolod <sevakul16@gmail.com>
2022-09-20 14:56:01 +0300

    Custom theme created

---
+++ src/theme/LibernetixTheme.jsx
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+
+const LiberTheme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#2D185C',
+    },
+    secondary: {
+      main: '#FF325D',
+    },
+    info: {
+      main: '#2196f3',
+    },
+  },
+})
+
+export default LiberTheme
