afb140118d8b5048f6f2c4adcf24a78316425ab5
Vsevolod <sevakul16@gmail.com>
2022-09-20 15:48:45 +0300

    Added fonts

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import NavigationBar from './components/NavigationBar/NavigationBar'
 import { ThemeProvider } from '@mui/material/styles';
 import LiberTheme from './theme/LibernetixTheme';
-
+import { Typography } from '@mui/material';
 function App() {
   return (
     <ThemeProvider theme={LiberTheme}>
       <NavigationBar />
+      <Typography sx={{fontFamily: 'Orbitron'}}>Hello world</Typography>
     </ThemeProvider>
   );
 }
